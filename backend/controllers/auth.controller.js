d6182f2440e9f820c0c5c8e3e02c1b3c96c5c4cd
unknown <khcharem.omar@gmail.com>
2026-06-11 00:38:20 +0100

    Fix: forgotPassword non-blocking email flow and remove await

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -211,7 +211,9 @@ exports.forgotPassword = async (req, res) => {
 
     // 3. Configuration du transporteur de mail
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+     host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
